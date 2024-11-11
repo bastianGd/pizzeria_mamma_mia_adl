01e9bb22f979ca0b0baf2d7c3f8c04a70f325fd1
Bastian Gabrielli <gabriellidoren@gmail.com>
2024-11-11 00:17:31 -0300

    creacion login

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,8 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Footer from "./components/footer/Footer";
 // import FormUser from "./components/register/FormUser";
 import NavbarPizza from "./components/navbar/NavbarPizza";
-import Login from "./components/login/Login";
+import Login from "./components/login/UserLog";
+import UserLog from "./components/login/UserLog";
 
 function App() {
   return (
@@ -13,7 +14,7 @@ function App() {
       <div className="app-container">
         {/* <Home/> */}
         {/* <FormUser /> */}
-        <Login/>
+        <UserLog/>
       </div>
       <Footer />
     </>
